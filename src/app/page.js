79ed56ef00462cee2813cda356758a2015f13439
jinayeom <jinayeom@tudents.cs.ubc.ca>
2025-08-07 13:33:19 -0700

    Added hovering titles and finished Create Website Page

--- src/app/page.js
+++ src/app/page.js
@@ -4,36 +4,36 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-start min-h-screen p-8 bg-white">
-      <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-gray-800 text-center mt-20">Full Stack Web Development Notes</h1>
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-gray-800 text-center mt-20 fade-in-up">Full Stack Web Development Notes</h1>
   
       <ul className="mt-12 space-y-4 text-xl text-gray-700">
       <li>
-          <Link href="/basics" className="hover:text-blue-600">
+          <Link href="/basics" className="button-link">
           How Servers and Browsers Interact
           </Link>
         </li>
         <li>
-          <Link href="/javascript" className="hover:text-blue-600">
+          <Link href="/javascript" className="button-link">
           JavaScript
           </Link>
         </li>
         <li>
-          <Link href="/jsx" className="hover:text-blue-600">
+          <Link href="/jsx" className="button-link">
           JSX
           </Link>
         </li>
         <li>
-          <Link href="/react" className="hover:text-blue-600">
+          <Link href="/react" className="button-link">
           React
           </Link>
         </li>
         <li>
-          <Link href="/nextjs" className="hover:text-blue-600">
+          <Link href="/nextjs" className="button-link">
           Next.js
           </Link>
         </li>
         <li>
-          <Link href="/website" className="hover:text-blue-600">
+          <Link href="/website" className="button-link">
           Creating a Website
           </Link>
         </li>
--- src/app/website/page.js
+++ src/app/website/page.js
@@ -2,24 +2,29 @@ import Link from "next/Link";
 export default function Home() {
     return (
       <main className="parent-container">
-        <h1 className="page-title">Creating a Website</h1>
+        <h1 className="page-title fade-in-up">Creating a Website</h1>
 
         {/* Author Note*/}
         <p className="author">Author: Jina</p>
 
         {/* Body Text */}
-        <p className="body">When I started my journey in web development, I was watching tons
-          of videos about full-stack web development - I was literally Googling "How to build
-          a website in 1 hour". It wasn't until I watched a handful of these videos that I 
-          started to realize that in order to know how to build a functional, reliable website,
-          I needed to learn the basics. Great things are built on strong, reliable foundations. 
-          <br/>
-          With that being said, this is a cheat sheet that I wished I had starting out. 
+        <p className="body">Before you use this guide, I want to preface that I am also 
+          just learning how to navigate web development. I simply created this website to share
+          some easy-to-read notes I've been creating along my journey. So please, use this with a 
+          grain of salt. I also want to share one piece of advice to you, from one beginner to another.
+          In the beginning of my learning journey,  I was Googling "How to build
+          a website in 1 hour" and trying to find the fastest way to build websites.
+          It wasn't until I watched a handful of these videos that I started to realize that in order to 
+          know how to build a functional, reliable website, I needed to learn the basics. Afterall, great things 
+          can only be built on strong, reliable foundations.
+        </p>
+        <br/>
+        <p className="body">With that being said, this is a cheat sheet that I wished I had starting out. 
           Do not just memorize this and plug it into ChatGPT and cross your fingers that you
           will build the next Facebook. Use it as a guideline and with a grain of salt.
-          <br/>
-          You have been warned!
         </p>
+        <br/>
+        <p className="body">You have been warned!</p>
         {/* List Section */}
         <br/>
 
@@ -111,22 +116,52 @@ export default function Home() {
 
       {/* Routing Pages */}
       <h3 className="subheading">Step 4: Connecting Your Pages</h3>
-      Once you've created your pages, it's time to start routing them to each other so
-      you can navigate between the different pages in your website. 
+      <br/>
+      <p className="body">Once you've created your pages, it's time to start routing them to each other so
+      you can navigate between the different pages in your website.
+      Let's assume you are trying to create buttons for the user to navigate from your
+      home page to the other pages in your website. For instance, an About page button on the Home menu.</p>
+
         <ol className="ordered-list">
           <li className="ordered-item">
             If you are navigating to a page inside your Next.js app (eg /home or /about page), 
             use the <code className="inline-code">&lt;Link&gt; &lt;/Link&gt;</code>.
             This tag is a React component provided by Next.js that allows you to conduct internal navigation. 
+          <br/>
+            Since we are trying to navigate to the About page, an internal page in your website, 
+            we will use <code className="inline-code">&lt;Link&gt; &lt;/Link&gt;</code>.  
           </li>
           <li className="ordered-item">
-            
+            Navigate to the <code className="inline-code">page.js</code> file for the page you want to navigate
+            away from. Once there, we will be creating a <code className="inline-code">&lt;Link&gt;</code> component
+            to the desired destination page.
           </li>
           <li className="ordered-item">
-            
-          </li>
-          <li className="ordered-item">
-            
+            The strcture will look like the following: 
+            <ol className="list-decimal list-inside ml-6 space-y-2 text-gray-700 text-base">
+              <li>
+                Write <code className="inline-code">&lt;Link&gt; &lt;/Link&gt;</code>
+              </li>
+              <li>
+                Inside the tag, write the name you want for the button that will navigate 
+                to the destination page (eg "About").
+              </li>
+              <li>
+                Inside the tag, write an <code className="inline-code">href="/destination"</code>,
+                replaciing the destination with the actual folder name for your destination page.
+              </li>
+              <li>
+                Include some styling as well via <code className="inline-code">className=...</code>
+                using Tailwind CSS.
+              </li>
+              <li>
+                Make sure that the capitalization and spelling of the /destination is the exact same 
+                as the folder in which your destination page exists. It is case-sensitive.
+              </li>
+              <li>
+                If you haven't already done so, write the custom Tailwind CSS class for your link router.
+              </li>
+            </ol>
           </li>
           </ol>
 
