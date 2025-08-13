50b7fc481c1f3d84d9e5e37e9df13ea24561753a
jinayeom <jinayeom@tudents.cs.ubc.ca>
2025-08-13 13:31:02 -0700

    Adjusted animations for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
       <div className="mt-12 flex flex-col md:flex-row items-start md:items-center gap-8">
         {/* Left: Image */}
         <div className="flex-shrink-0">
-          <h2 className="subheading">Full Stack Summary</h2>
-          <Image
+          <h2 className="subheading fade-in-up">Full Stack Summary</h2>
+          <Image className="fade-in-up"
             src="/images/summary.png"
             alt="Summary Image of Full Stack Web Development"
             width={600}
@@ -22,37 +22,37 @@ export default function Home() {
         <div>
           <ul className="mt-12 flex flex-col items-left space-y-4 text-xl text-gray-700">
           <li>
-              <Link href="/basics" className="button-link">
+              <Link href="/basics" className="button-link fade-in-up">
               How Servers and Browsers Interact
               </Link>
             </li>
             <li>
-              <Link href="/javascript" className="button-link">
+              <Link href="/javascript" className="button-link fade-in-up">
               JavaScript
               </Link>
             </li>
             <li>
-              <Link href="/jsx" className="button-link">
+              <Link href="/jsx" className="button-link fade-in-up">
               JSX
               </Link>
             </li>
             <li>
-              <Link href="/react" className="button-link">
+              <Link href="/react" className="button-link fade-in-up">
               React
               </Link>
             </li>
             <li>
-              <Link href="/nextjs" className="button-link">
+              <Link href="/nextjs" className="button-link fade-in-up">
               Next.js
               </Link>
             </li>
             <li>
-              <Link href="/website" className="button-link">
+              <Link href="/website" className="button-link fade-in-up">
               Creating a Website
               </Link>
             </li>
             <li>
-              <Link href="/fetch" className="button-link">
+              <Link href="/fetch" className="button-link fade-in-up">
               Fetching Data
               </Link>
             </li>
